190cd30dce029a4c2f9ca30aa7c03b07e2d2fd75
Adit122022 <adity202212@gmail.com>
2025-01-24 09:19:33 +0530

    version 002

---
+++ BACKEND_PROJECTS/PROJECTS/GOGGLE DRIVE/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const userRouter =require('./routes/user.routes')
+const dotenv= require('dotenv');
+const connectToDB =require('./config/db')
+dotenv.config();
+connectToDB();
+const app =express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+app.use('/user',userRouter)
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000");  
+})
