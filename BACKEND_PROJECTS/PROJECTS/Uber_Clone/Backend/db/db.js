36bfc88621ef17f8a8ce53f6d692f017ebe07ef0
Adit122022 <adity202212@gmail.com>
2025-01-27 20:37:56 +0530

    version 001 - > 02  folder structure

---
+++ BACKEND_PROJECTS/PROJECTS/Uber_Clone/Backend/db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log('MongoDB Connected...✅✅✅'))
+        .catch(err => console.log(err));
+}
+
+module.exports = connect;
