59e87657ae37f75044c37b638038b34d0e85141f
Adit122022 <adity202212@gmail.com>
2025-01-31 11:52:17 +0530

    version 004 --> strcuture maintainence

--- BACKEND_PROJECTS/PROJECTS/Uber_Clone/Frontend/src/pages/Home.jsx
+++ BACKEND_PROJECTS/PROJECTS/Uber_Clone/Frontend/src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import black from '/public/uber-logo-black.png'
+import mapImage from '/public/map-image.webp'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='h-screen w-screen relative'>
+      <img className=' object-cover w-16 absolute left-5 top-5' src={black} alt="Uber Logo" />
+      <div className="h-screen w-screen">
+        {/* image for temporary image */}
+        <img className='h-full w-full object-cover' src={mapImage} alt="" />
+      </div>
+      <div className='bg-white p-5 absolute top-0'>
+        <h4>Find a Trip</h4>
+        <form action="">
+          <input type="text" placeholder='Enter Pickup Location' />
+          <input type="text" placeholder='Enter Dropoff Location' />
+          <button>Find a Trip</button>
+        </form>
+      </div>
+    
+    </div>
   )
 }
 
