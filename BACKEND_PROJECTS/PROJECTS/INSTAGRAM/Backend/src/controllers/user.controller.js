2bf072e069c25c1b9b3baffd786761823b2ec228
Adit122022 <adity202212@gmail.com>
2025-02-22 11:56:28 +0530

    creating a user

--- BACKEND_PROJECTS/PROJECTS/INSTAGRAM/Backend/src/controllers/user.controller.js
+++ BACKEND_PROJECTS/PROJECTS/INSTAGRAM/Backend/src/controllers/user.controller.js
@@ -1,12 +1,16 @@
 import { validationResult } from "express-validator"
 import userModel from "../models/user.js"
+import { createUser } from "../services/user.service.js";
 
 
 export const createUserController  = async (req ,res) =>{
     try{  
  const errors = validationResult(req);
  if(!errors.isEmpty()) return res.status(400).json({message :errors.array()});
- res.send({message : " regsiter"})
+ const {username ,email, password } = req.body 
+ const user = await createUser( {username,email,password });  // service
+ const token = user.generateToken();
+ res.status(201).json({user ,token});
     }catch(err){
         console.error(err)
         res.status(500).json({message: "Server Error"})
--- BACKEND_PROJECTS/PROJECTS/INSTAGRAM/Backend/src/services/user.service.js
+++ BACKEND_PROJECTS/PROJECTS/INSTAGRAM/Backend/src/services/user.service.js
@@ -1,7 +1,13 @@
 import userModel from "../models/user";
 
-const createUser = async({username, email, password}) =>{
+export const createUser = async({username, email, password}) =>{
     try{
-        if(!username  || !email || !password) throw new Error("Invalid username or email")
-    }
+        if(!username  || !email || !password) throw new Error("All fields are required [ username , emial , password ]");
+        const isUserAlreadyExists = await userModel.findOne({$or:[{username},{email}]});
+        if(isUserAlreadyExists) throw new Error("User already exists with this email or username");
+        const hashedPassword = await userModel.hashedPassword(password);
+        const user = await userModel.create({username ,email , password :hashedPassword});
+        delete user._doc.password;  // for safety 
+        return user;
+    }catch(err){ console.log(err);}
 }
