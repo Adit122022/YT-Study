3ce8cba88cdcae41615a66132d34b859ebe3059a
Adit122022 <adity202212@gmail.com>
2025-04-10 03:14:27 +0530

    feat: Add Theme component for dark/light mode toggle and update Navbar to use it

--- BACKEND_PROJECTS/PROJECTS/CHAT_APP/Frontend/src/components/Navbar.jsx
+++ BACKEND_PROJECTS/PROJECTS/CHAT_APP/Frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import {  AtSign, LogOut, Settings, User } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuthStore } from '../store/useAuthStore'
+import Theme from '../assets/Theme'
 
 
 
@@ -17,7 +18,8 @@ const Navbar = () => {
           <div className="flex items-center gap-8">
             <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
-                <AtSign  className="w-5 h-5 text-primary" />
+                {/* <AtSign  className="w-5 h-5 text-primary" /> */}
+                <Theme/>
               </div>
               <h1 className="text-lg font-bold">Annova</h1>
             </Link>
